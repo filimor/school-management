8f4cff04b9863d35ac399fc8dee5a3be6b00cfd3
Filipe Moreira <filimor@posteo.com.br>
2022-07-13 02:19:31 -0300

    Create the domain entities Student and Address (#6)
    * Implement Student Model
    
    * Add Address Model
    
    * Refactor code adding the Entity Model
    
    * Add UML Component Diagram
    
    * Add UML Component Diagram
    
    * Add custom Exception and Validation to the Models
    
    * Refactor Address validation
    
    * Refactor Student validation
    
    * Finish the first implementation of the models
    
    * Fix conflicts
    
    * Fix conflicts

---
+++ src/SchoolManagement.Domain.Tests/AddressTest.cs
@@ -0,0 +1,274 @@
+﻿using FluentAssertions;
+using SchoolManagement.Domain.Exceptions;
+using SchoolManagement.Domain.Models;
+using SchoolManagement.Tests.ClassData;
+
+namespace SchoolManagement.Tests;
+
+public class AddressTest
+{
+    [Fact]
+    public void Should_Create_New_Address_With_Required_Fields()
+    {
+        var address = new Address(
+            1,
+            "Rua Jequitibá",
+            "123",
+            "Centro",
+            zipCode: "12345-678"
+        );
+        address.Should().NotBeNull();
+    }
+
+    [Theory]
+    [MemberData(nameof(BrazilianStates))]
+    public void Should_Create_Address_With_All_Brazilian_States(string state)
+    {
+        var address = new Address(
+            1,
+            "Rua Jequitibá",
+            "123",
+            "Centro",
+            "São Paulo",
+            state,
+            "12345-678"
+        );
+        address.Should().NotBeNull();
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Should_Not_Create_Address_With_Invalid_Id(int id)
+    {
+        var act = () => new Address(
+            id,
+            "Rua Jequitibá",
+            "123",
+            "Centro",
+            "São Paulo",
+            "SP",
+            "012345-678",
+            "Apto 101"
+        );
+
+        act.Should().Throw<DomainException>();
+    }
+
+    [Theory]
+    [ClassData(typeof(InvalidStringsClassData))]
+    public void Should_Not_Create_Address_With_Invalid_Street(string street)
+    {
+        var act = () => new Address(
+            1,
+            street,
+            "123",
+            "Centro",
+            "São Paulo",
+            "SP",
+            "012345-678",
+            "Apto 101"
+        );
+
+        act.Should().Throw<DomainException>();
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidAddressNumbers))]
+    public void Should_Not_Create_Address_With_Invalid_Number(string number)
+    {
+        var act = () => new Address(
+            1,
+            "Rua Jequitibá",
+            number,
+            "Centro",
+            "São Paulo",
+            "SP",
+            "012345-678",
+            "Apto 101"
+        );
+
+        act.Should().Throw<DomainException>();
+    }
+
+    [Theory]
+    [ClassData(typeof(InvalidStringsClassData))]
+    public void Should_Not_Create_Address_With_Invalid_District(string district)
+    {
+        var act = () => new Address(
+            1,
+            "Rua Jequitibá",
+            "123",
+            district,
+            "São Paulo",
+            "SP",
+            "012345-678",
+            "Apto 101"
+        );
+
+        act.Should().Throw<DomainException>();
+    }
+
+    [Theory]
+    [ClassData(typeof(InvalidStringsClassData))]
+    public void Should_Not_Create_Address_With_Invalid_City(string city)
+    {
+        var act = () => new Address(
+            1,
+            "Rua Jequitibá",
+            "123",
+            "Centro",
+            city,
+            "SP",
+            "012345-678",
+            "Apto 101"
+        );
+
+        act.Should().Throw<DomainException>();
+    }
+
+    [Theory]
+    [ClassData(typeof(InvalidStringsClassData))]
+    public void Should_Not_Create_Address_With_Invalid_State(string state)
+    {
+        var act = () => new Address(
+            1,
+            "Rua Jequitibá",
+            "123",
+            "Centro",
+            "São Paulo",
+            state,
+            "012345-678",
+            "Apto 101"
+        );
+
+        act.Should().Throw<DomainException>();
+    }
+
+    [Theory]
+    [InlineData("123456-789")]
+    [InlineData("1234-567")]
+    [InlineData("1234-5678")]
+    [InlineData("123456789")]
+    [InlineData("1234567")]
+    [InlineData("12345678")]
+    [InlineData("1234567a")]
+    public void Should_Not_Create_Address_With_Invalid_ZipCode(string zipCode)
+    {
+        var act = () => new Address(
+            1,
+            "Rua Jequitibá",
+            "123",
+            "Centro",
+            "São Paulo",
+            "SP",
+            zipCode,
+            "Apto 101"
+        );
+
+        act.Should().Throw<DomainException>();
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidAddressComplements))]
+    public void Should_Not_Create_Address_With_Invalid_Street2(string street2)
+    {
+        var act = () => new Address(
+            1,
+            "Rua Jequitibá",
+            "123",
+            "Centro",
+            "São Paulo",
+            "SP",
+            "012345-678",
+            street2
+        );
+
+        act.Should().Throw<DomainException>();
+    }
+
+    [Fact]
+    public void Should_Be_Able_To_Update_Address_Data_With_Same_Id()
+    {
+        var address = new Address(
+            1,
+            "Rua Jequitibá",
+            "123",
+            "Centro",
+            "São Paulo",
+            "SP",
+            "01234-567",
+            "Apto 101"
+        );
+
+        address.Update("Rua Beija-Flor",
+            "456",
+            "Jardins",
+            "Belo Horizonte",
+            "MG",
+            "02345-678",
+            "Apto 202");
+
+        address.Id.Should().Be(1);
+        address.Street.Should().Be("Rua Beija-Flor");
+        address.Number.Should().Be("456");
+        address.District.Should().Be("Jardins");
+        address.City.Should().Be("Belo Horizonte");
+        address.State.Should().Be("MG");
+        address.ZipCode.Should().Be("02345-678");
+        address.Street2.Should().Be("Apto 202");
+    }
+
+    private static IEnumerable<object[]> BrazilianStates()
+    {
+        return new[]
+        {
+            new object[] { "AC" },
+            new object[] { "AL" },
+            new object[] { "AP" },
+            new object[] { "AM" },
+            new object[] { "BA" },
+            new object[] { "CE" },
+            new object[] { "DF" },
+            new object[] { "ES" },
+            new object[] { "GO" },
+            new object[] { "MA" },
+            new object[] { "MT" },
+            new object[] { "MS" },
+            new object[] { "MG" },
+            new object[] { "PA" },
+            new object[] { "PB" },
+            new object[] { "PR" },
+            new object[] { "PE" },
+            new object[] { "PI" },
+            new object[] { "RJ" },
+            new object[] { "RN" },
+            new object[] { "RS" },
+            new object[] { "RO" },
+            new object[] { "RR" },
+            new object[] { "SC" },
+            new object[] { "SP" },
+            new object[] { "SE" },
+            new object[] { "TO" }
+        };
+    }
+
+    private static IEnumerable<object[]> InvalidAddressNumbers()
+    {
+        return new[]
+        {
+            new object[] { "" },
+            new object[] { " " },
+            new object[] { null! },
+            new object[] { new string('a', 10 + 1) }
+        };
+    }
+
+    private static IEnumerable<object[]> InvalidAddressComplements()
+    {
+        return new[]
+        {
+            new object[] { new string('a', 50 + 1) }
+        };
+    }
+}
---
+++ src/SchoolManagement.Domain/Models/Address.cs
@@ -0,0 +1,94 @@
+﻿using System.Text.RegularExpressions;
+using SchoolManagement.Domain.Validations;
+
+namespace SchoolManagement.Domain.Models;
+
+public sealed class Address : Entity
+{
+    private static readonly string[] States =
+    {
+        "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG",
+        "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"
+    };
+
+    public Address(int id, string street, string number, string district, string city = "São Paulo",
+        string state = "SP", string zipCode = "", string street2 = "") : base(id)
+    {
+        Initialize(street, number, district, city, state, zipCode, street2);
+    }
+
+    public Address(string street, string number, string district, string city = "São Paulo",
+        string state = "SP", string zipCode = "", string street2 = "")
+    {
+        Initialize(street, number, district, city, state, zipCode, street2);
+    }
+
+
+    public string Street { get; private set; } = null!;
+
+    public string Number { get; private set; } = null!;
+
+    public string Street2 { get; private set; } = null!;
+
+    public string District { get; private set; } = null!;
+
+    public string City { get; private set; } = null!;
+
+    public string State { get; private set; } = null!;
+
+    public string ZipCode { get; private set; } = null!;
+
+    public Student? Student { get; set; }
+
+    public void Update(string street, string number, string district, string city, string state, string zipCode,
+        string street2)
+    {
+        Initialize(street, number, district, city, state, zipCode, street2);
+        Update();
+    }
+
+    private void Initialize(string street, string number, string district, string city, string state, string zipCode,
+        string street2)
+    {
+        Validate(street, number, district, city, state, zipCode, street2);
+        Street = street;
+        Number = number;
+        Street2 = street2;
+        District = district;
+        City = city;
+        State = state;
+        ZipCode = zipCode;
+    }
+
+    private static void Validate(string street, string number, string district, string city, string state,
+        string zipCode, string street2)
+    {
+        DomainValidation.When(string.IsNullOrWhiteSpace(street),
+            "O Logradouro é obrigatório e não pode ter um valor vazio.");
+        DomainValidation.When(street.Length is < 5 or > 100,
+            "O Logradouro deve ter entre 5 e 100 caracteres.");
+
+        DomainValidation.When(string.IsNullOrWhiteSpace(district),
+            "O Bairro é obrigatório e não pode ter um valor vazio.");
+        DomainValidation.When(district.Length is < 5 or > 100,
+            "O Bairro deve ter entre 5 e 100 caracteres.");
+
+        DomainValidation.When(string.IsNullOrWhiteSpace(city),
+            "A cidade é obrigatória e não pode ter um valor vazio.");
+        DomainValidation.When(city.Length is < 5 or > 100,
+            "A cidade deve ter entre 5 e 100 caracteres.");
+
+        DomainValidation.When(string.IsNullOrWhiteSpace(number),
+            "O Número é obrigatório e não pode ter um valor vazio.");
+        DomainValidation.When(number.Length is < 1 or > 10,
+            "O Número é obrigatório e deve ter entre 1 e 10 caracteres.");
+
+        DomainValidation.When(!States.Contains(state),
+            "O Estado é obrigatório e deve ter um valor válido com 2 caracteres.");
+        DomainValidation.When(street2.Length > 50, "O complemento deve possuir no máximo 50 caracteres.");
+
+        var regex = new Regex(@"^([0-9]{5}-[\d]{3})$");
+        DomainValidation.When(!regex.IsMatch(zipCode),
+            "O CEP é obrigatório e deve possuir 5 dígitos, um traço e mais 3 dígitos.");
+    }
+}
